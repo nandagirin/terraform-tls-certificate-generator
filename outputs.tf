ba732e6b2bbfd430a3980373d32413e1212b06f1
nanda.girindratama <nanda.girindratama@byorange.co.id>
2022-02-13 19:27:11 +0700

    Provide ca and cert key as output with sensitive set to true

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,13 @@ output "cert_request_pem" {
 output "cert_signed_pem" {
   value = tls_locally_signed_cert.cert_signed.cert_pem
 }
+
+output "ca_key_pem" {
+  sensitive = true
+  value     = tls_private_key.ca_key.private_key_pem
+}
+
+output "cert_key_pem" {
+  sensitive = true
+  value     = tls_private_key.cert_key.private_key_pem
+}
