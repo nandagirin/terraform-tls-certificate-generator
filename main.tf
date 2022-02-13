6e8aa5f28a0fdb76b9140c373900915b1f699e07
nanda.girindratama <nanda.girindratama@byorange.co.id>
2022-02-14 05:50:58 +0700

    Apply CSR toggle as Terraform conditional in all resources

--- main.tf
+++ main.tf
@@ -2,14 +2,18 @@
   Generate CA key and CA certificate
 */
 resource "tls_private_key" "ca_key" {
+  count = var.generate_cert_sign_request_only ? 0 : 1
+
   algorithm   = var.tls_private_key_algorithm
   ecdsa_curve = var.tls_private_key_algorithm == "ECDSA" ? var.tls_private_key_ecdsa_curve : null
   rsa_bits    = var.tls_private_key_algorithm == "RSA" ? var.tls_private_key_rsa_bits : null
 }
 
 resource "tls_self_signed_cert" "ca_cert" {
-  key_algorithm   = tls_private_key.ca_key.algorithm
-  private_key_pem = tls_private_key.ca_key.private_key_pem
+  count = var.generate_cert_sign_request_only ? 0 : 1
+
+  key_algorithm   = tls_private_key.ca_key[0].algorithm
+  private_key_pem = tls_private_key.ca_key[0].private_key_pem
 
   subject {
     common_name         = trimprefix(var.tls_cert_subject["common_name"], "*.")
@@ -58,10 +62,12 @@ resource "tls_cert_request" "cert_request" {
   Generate signed certificate from generated CA and CSR
 */
 resource "tls_locally_signed_cert" "cert_signed" {
+  count = var.generate_cert_sign_request_only ? 0 : 1
+
   cert_request_pem      = tls_cert_request.cert_request.cert_request_pem
-  ca_key_algorithm      = tls_private_key.ca_key.algorithm
-  ca_private_key_pem    = tls_private_key.ca_key.private_key_pem
-  ca_cert_pem           = tls_self_signed_cert.ca_cert.cert_pem
+  ca_key_algorithm      = tls_private_key.ca_key[0].algorithm
+  ca_private_key_pem    = tls_private_key.ca_key[0].private_key_pem
+  ca_cert_pem           = tls_self_signed_cert.ca_cert[0].cert_pem
   validity_period_hours = var.tls_cert_validity_period_hours
   allowed_uses          = var.tls_cert_allowed_uses
   is_ca_certificate     = false
@@ -73,14 +79,14 @@ resource "tls_locally_signed_cert" "cert_signed" {
   Generate certificates and keys as local files
 */
 resource "local_file" "ca_crt" {
-  count    = var.generate_certs_keys_as_local_files ? 1 : 0
-  content  = tls_self_signed_cert.ca_cert.cert_pem
+  count    = var.generate_certs_keys_as_local_files && !var.generate_cert_sign_request_only ? 1 : 0
+  content  = tls_self_signed_cert.ca_cert[0].cert_pem
   filename = "./certs/ca-cert.pem"
 }
 
 resource "local_file" "ca_key" {
-  count    = var.generate_certs_keys_as_local_files ? 1 : 0
-  content  = tls_private_key.ca_key.private_key_pem
+  count    = var.generate_certs_keys_as_local_files && !var.generate_cert_sign_request_only ? 1 : 0
+  content  = tls_private_key.ca_key[0].private_key_pem
   filename = "./certs/ca-key.pem"
 }
 
@@ -91,13 +97,13 @@ resource "local_file" "cert_request" {
 }
 
 resource "local_file" "cert_signed" {
-  count    = var.generate_certs_keys_as_local_files ? 1 : 0
-  content  = tls_locally_signed_cert.cert_signed.cert_pem
+  count    = var.generate_certs_keys_as_local_files && !var.generate_cert_sign_request_only ? 1 : 0
+  content  = tls_locally_signed_cert.cert_signed[0].cert_pem
   filename = "./certs/cert-signed.pem"
 }
 
 resource "local_file" "cert_key" {
-  count    = var.generate_certs_keys_as_local_files ? 1 : 0
+  count    = var.generate_certs_keys_as_local_files && !var.generate_cert_sign_request_only ? 1 : 0
   content  = tls_private_key.cert_key.private_key_pem
   filename = "./certs/cert-key.pem"
 }
